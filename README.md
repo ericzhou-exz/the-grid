91ff728e05a82a42f4fe58dae0b17a0435ff26bf
Eric Zhou <ericzhou.exz@gmail.com>
2023-04-07 17:35:47 +1000

    added Card interface, getWords, generateCards

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
 # The Grid
 This is an app used for word association game, "The Grid".
 
+- Clone using git.
+- Run ```npm install``` to install dependencies.
+- Run ```npm start``` to start the development server.
+
 ## User guide
 Add 5 sets of 4 related words and 5 extra dummy words
 
 (Build in Electron so that it can run as a separate executable?)
 
 ## TODO
+### Design Specification
+- A 5x5 grid of cards is rendered.
+- 5 sets of 4 are associated words. 1 set of 5 is unrelated.
+- When a card is selected it turns red.
+- Select 5 cards in a row. If those 5 are associated, they are "solved".
+- Mark them as green and they can no longer be selected.
+- If they are not associated, unselect the cards.
 
 ### Features to be implemented
-- 5x5 grid, 5 sets of 4 and 5 dummy cards.
+- Render a 5x5 grid.
+- Selection and solving logic.
+- Shuffle cards randomly each time.
+- UI to add words before the game starts.
 - Inbuilt timer
 - Inbuilt scoring (this may also be a separate app/plugin for better integration with Slides)
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
 import React, { useState } from 'react';
 import './index.css'
 
-/*
-class Card {
-    private id: number;
-    private value: string;
-    private isFlipped: boolean;
-
-    constructor(value: string, id: number) {
-        this.value = value;
-        this.id = id;
-        this.isFlipped = false;
-    }
+interface Card {
+    id: number;
+    value: string;
+    isFlipped: boolean;
 }
 
-function generateCards = (): Card[] => {
-    const cards: Card = [];
-
-
+function getWords(): string[] {
+    const words: string[] = [];
     for (let i = 0; i < 25; i++) {
-        cards.push(card);
+        words.push("foo");
+    }
+    return words;
+}
+
+function generateCards(): Card[] {
+    const words = getWords();
+    
+    const cards: Card[] = [];
+    let id = 0;
+    for (let i = 0; i < 25; i++) {
+        const newCard: Card = {
+            id: i,
+            value: words[i],
+            isFlipped: false,
+        };
+        cards.push(newCard);
     }
     return cards;
+}
+
+function handleCardClick(col: number, row: number) {
+    // TODO
 }
 /*
 function shuffleCards(cards): typeof cards {
@@ -30,31 +41,24 @@ function shuffleCards(cards): typeof cards {
 */
 
 function App() {
-    const [cards, setCards] = useState<Array<string>>([]);
-    const [flipped, setFlipped] = useState<Array<number>>([]);
-    const [solved, setSolved] = useState<Array<number>>([]);
+    //const [cards, setCards] = useState<Array<string>>([]);
+    //const [flipped, setFlipped] = useState<Array<number>>([]);
+    //const [solved, setSolved] = useState<Array<number>>([]);
     
-    function handleCardClick(col: number, row: number) {
-        // TODO
-    }
-
-
+    const cards = generateCards();
+    //console.log(cards[2].value);
 
     function renderCards() {
-        const cards = [];
-        let id = 0;
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-                const card = (
-                    <button className="card" onClick={() => handleCardClick(i, j)}>
-                        {id}
-                    </button>
-                );
-                cards.push(card);
-                id++;
-            }
+        const buttons = [];
+        for (let i = 0; i < 25; i++) {
+            const button = (
+            <button className="card">
+                {cards[i].value}
+            </button>
+            );
+        buttons.push(button);
         }
-        return <div className="card-grid">{cards}</div>;
+        return <div className="card-grid">{buttons}</div>;
     }
 
     return (
