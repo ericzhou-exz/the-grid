a058f07ed36393e402f9d36125dd29021c8bd67b
Eric Zhou <ericzhou.exz@gmail.com>
2023-05-13 17:53:10 +1000

    moved stuff into Card class. Need to make some functions non-static

--- src/App.tsx
+++ src/App.tsx
@@ -11,50 +11,54 @@ class Card {
         this.value = value;
         this.isFlipped = isFlipped;
     }
-}
 
-function getWords(): string[] {
-    const words: string[] = [];
-    for (let i = 0; i < 25; i++) {
-        words.push("card");
+    public static shuffle(cards: Card[]): Card[] {
+        let shuffledCards: Card[] = [];
+        for (let i = 0; i < 25; i++) {
+            const randIndex = Math.floor(Math.random() * cards.length);
+            const randCard: Card = cards[randIndex];
+            shuffledCards.push(randCard);
+            cards.splice(randIndex, 1)
+        }
+        return shuffledCards;
     }
-    return words;
-}
 
-function generateCards(words: string[]): Card[] {    
-    const cards: Card[] = [];
-    for (let i = 0; i < 25; i++) {
-        const newCard = new Card(i, words[i], false);
-        cards.push(newCard);
+    public static generate(): Card[] {
+        const words = Card.getWords();  
+        const cards: Card[] = [];
+        for (let i = 0; i < 25; i++) {
+            const newCard = new Card(i, words[i], false);
+            cards.push(newCard);
+        }
+        return cards;
     }
-    return cards;
-}
 
-function shuffleCards(cards: Card[]): Card[] {
-    let shuffledCards: Card[] = [];
-    for (let i = 0; i < 25; i++) {
-        const randIndex = Math.floor(Math.random() * cards.length);
-        const randCard: Card = cards[randIndex];
-        shuffledCards.push(randCard);
-        cards.splice(randIndex, 1)
+    private static getWords(): string[] {
+        const words: string[] = [];
+        for (let i = 0; i < 25; i++) {
+            words.push("card");
+        }
+        return words;
     }
-    return shuffledCards;
-}
 
-function renderCards(cards: Card[]) {
-    const buttons = [];
-    for (let i = 0; i < 25; i++) {
-        const button = (
-        <button className="card">
-            {cards[i].value + " " + cards[i].id + " " + cards[i].isFlipped}
-        </button>
-        );
-    buttons.push(button);
+    public static render(cards: Card[]) {
+        const buttons = [];
+        for (let i = 0; i < 25; i++) {
+            const button = (
+            <button className="card">
+                {cards[i].value + " " + cards[i].id + " " + cards[i].isFlipped}
+            </button>
+            );
+        buttons.push(button);
+        }
+        return <div className="card-grid">{buttons}</div>;
     }
-    return <div className="card-grid">{buttons}</div>;
 }
 
-function handleCardClick(col: number, row: number) {
+
+
+function handleCardClick(id: number) {
+
     // TODO
 }
 
@@ -63,13 +67,11 @@ function App() {
     //const [flipped, setFlipped] = useState<Array<number>>([]);
     //const [solved, setSolved] = useState<Array<number>>([]);
     
-    const cards = generateCards(getWords());
-    const shuffledCards = shuffleCards(cards);
-    
+    const shuffledCards = Card.shuffle(Card.generate());
 
     return (
         <div className='container'>
-            {renderCards(shuffledCards)}
+            {Card.render(shuffledCards)}
         </div>
     );
 }
